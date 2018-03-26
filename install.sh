444a64bea367d18996a06016220e00d8d7ecf5fc
Kai Guo <kguo@fortinet.com>
2018-03-26 12:18:24 -0700

    Add scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# https://www.networkworld.com/article/2703490/operating-systems/unix---when-a-bash-script-asks--where-am-i--.html
+PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -s $PWD"/scripts*" ~/bin
