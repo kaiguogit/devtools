ba0c1e5dea809e738c620437350d723b36d9e94d
Kai Guo <kguo@fortinet.com>
2018-11-06 09:33:06 -0800

    Change copy-pwd to copypwd

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 # https://www.networkworld.com/article/2703490/operating-systems/unix---when-a-bash-script-asks--where-am-i--.html
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s $PWD"/scripts*" ~/bin
+ln -s $PWD/scripts/* ~/bin/
